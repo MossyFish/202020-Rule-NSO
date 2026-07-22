715fbe3e7b3637b0ac0e067d29474005596f4c4d
flyingf15h <wangtszfei@gmail.com>
2026-07-22 11:26:32 +0800

    adding a .exe

--- ui_elements.py
+++ ui_elements.py
@@ -4,6 +4,7 @@ import ctypes
 import os
 import tkinter as tk 
 from PIL import Image, ImageDraw, ImageTk, ImageFont
+import sys
 
 # Pixel font used in NSO 
 PIXEL_FONT_FAMILY = "PixelMplus10"
@@ -11,15 +12,18 @@ PIXEL_FONT_FAMILY = "PixelMplus10"
 # Font load 
 def _load_pixel_fonts():
     try:
-        base_dir = os.path.dirname(os.path.abspath(__file__))
+        if getattr(sys, "frozen", False):
+            base_dir = sys._MEIPASS
+        else:
+            base_dir = os.path.dirname(os.path.abspath(__file__))
         fonts_dir = os.path.join(base_dir, "fonts")
-        FR_PRIVATE = 0x10 
+        FR_PRIVATE = 0x10
         for fname in ("PixelMplus10-Regular.ttf", "PixelMplus10-Bold.ttf"):
-            path = os.path.join(fonts_dir, fname) 
+            path = os.path.join(fonts_dir, fname)
             if os.path.exists(path):
                 ctypes.windll.gdi32.AddFontResourceExW(ctypes.c_wchar_p(path), FR_PRIVATE, 0)
     except Exception:
-        pass 
+        pass
 
 def pixel_font(size, bold=False):
     px = round(size * 96/72)
